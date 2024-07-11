ea37fde488196ad62138da51c46c91e125a074c7
csmplatform <csmplatform@users.noreply.github.com>
2024-07-11 14:26:38 +0000

    Automatically update typescript client (ref: Cosmo-Tech/cosmotech-api@3346fb3acc0778d834a9b4542cfe816e27b0f06e)
    Co-authored-by: sjoubert <sjoubert@users.noreply.github.com>

--- api.ts
+++ api.ts
@@ -2839,44 +2839,6 @@ export const ConnectorApiAxiosParamCreator = function (configuration?: Configura
         },
         /**
          * 
-         * @summary Get the details of a connector
-         * @param {string} connectorName the Connector name
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        findConnectorByName: async (connectorName: string, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
-            // verify required parameter 'connectorName' is not null or undefined
-            assertParamExists('findConnectorByName', 'connectorName', connectorName)
-            const localVarPath = `/connectors/name/{connector_name}`
-                .replace(`{${"connector_name"}}`, encodeURIComponent(String(connectorName)));
-            // use dummy base URL string because the URL constructor only accepts absolute URLs.
-            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
-            let baseOptions;
-            if (configuration) {
-                baseOptions = configuration.baseOptions;
-            }
-
-            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
-            const localVarHeaderParameter = {} as any;
-            const localVarQueryParameter = {} as any;
-
-            // authentication oAuth2AuthCode required
-            // oauth required
-            await setOAuthToObject(localVarHeaderParameter, "oAuth2AuthCode", [], configuration)
-
-
-    
-            setSearchParams(localVarUrlObj, localVarQueryParameter);
-            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
-            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
-
-            return {
-                url: toPathString(localVarUrlObj),
-                options: localVarRequestOptions,
-            };
-        },
-        /**
-         * 
          * @summary Register a new connector
          * @param {Connector} connector the Connector to register
          * @param {*} [options] Override http request option.
@@ -2992,19 +2954,6 @@ export const ConnectorApiFp = function(configuration?: Configuration) {
         },
         /**
          * 
-         * @summary Get the details of a connector
-         * @param {string} connectorName the Connector name
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        async findConnectorByName(connectorName: string, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Connector>> {
-            const localVarAxiosArgs = await localVarAxiosParamCreator.findConnectorByName(connectorName, options);
-            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
-            const localVarOperationServerBasePath = operationServerMap['ConnectorApi.findConnectorByName']?.[localVarOperationServerIndex]?.url;
-            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
-        },
-        /**
-         * 
          * @summary Register a new connector
          * @param {Connector} connector the Connector to register
          * @param {*} [options] Override http request option.
@@ -3062,16 +3011,6 @@ export const ConnectorApiFactory = function (configuration?: Configuration, base
         },
         /**
          * 
-         * @summary Get the details of a connector
-         * @param {string} connectorName the Connector name
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        findConnectorByName(connectorName: string, options?: any): AxiosPromise<Connector> {
-            return localVarFp.findConnectorByName(connectorName, options).then((request) => request(axios, basePath));
-        },
-        /**
-         * 
          * @summary Register a new connector
          * @param {Connector} connector the Connector to register
          * @param {*} [options] Override http request option.
@@ -3123,18 +3062,6 @@ export class ConnectorApi extends BaseAPI {
      */
     public findConnectorById(connectorId: string, options?: RawAxiosRequestConfig) {
         return ConnectorApiFp(this.configuration).findConnectorById(connectorId, options).then((request) => request(this.axios, this.basePath));
-    }
-
-    /**
-     * 
-     * @summary Get the details of a connector
-     * @param {string} connectorName the Connector name
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     * @memberof ConnectorApi
-     */
-    public findConnectorByName(connectorName: string, options?: RawAxiosRequestConfig) {
-        return ConnectorApiFp(this.configuration).findConnectorByName(connectorName, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
